55baae81fe6f3cfadb2eee10f57138f9e89065e9
mjp <mike.pekala@jhuapl.edu>
2016-03-30 15:26:47 -0400

    updated

--- examples/make.sh
+++ examples/make.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-ipython nbconvert --to pdf --template latex_nocode nist_rand_metal01.ipynb
+#ipython nbconvert --to pdf --template latex_nocode nist_rand_metal01.ipynb
+ipython nbconvert --to pdf --template latex_nocode rotation_question.ipynb
