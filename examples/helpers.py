ae10d206115d1d273479cb0e020ab80ec96bd5e4
mjp <mike.pekala@jhuapl.edu>
2016-04-02 12:11:07 -0400

    removed load_data

--- examples/helpers.py
+++ examples/helpers.py
@@ -8,28 +8,6 @@ import pylab as plt
 from mpl_toolkits.mplot3d import axes3d, art3d
 
 
-def load_data(fn):
-    """Loads data set from CSV file.
-       fn : The .csv file to load.  Format is assumed to be:
-            xNominal, xMeasured, yNominal, yMeasured, dNominal, dMeasured, zNominal, zMeasured
-            
-            where x, y, d, z are pin x-position, y-position, diameter and height.
-    """
-    Z = []
-
-    cast = lambda row: [row[0].strip(),] + [float(x) for x in row[1:]]
-    
-    with open(fn, 'rU') as f:
-        reader = csv.reader(f, delimiter=',')
-        for rowIdx, row in enumerate(reader):
-            if rowIdx == 0: continue # skip header
-                
-            pinId, xNom, xMeas, yNom, yMeas, dNom, dMeas, zNom, zMeas = cast(row)
-            Z.append((xNom, xMeas, yNom, yMeas, dNom, dMeas, zNom, zMeas))
-
-    return np.array(Z)
-
-
 def cart2polar(x, y):
     "Cartesian to polar coordinates."
     r = np.sqrt(x**2 + y**2)
