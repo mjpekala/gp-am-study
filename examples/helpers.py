a6904c3061d367523e5b9bd2231c4d2d08966f2d
mjp <mike.pekala@jhuapl.edu>
2016-04-06 15:27:18 -0400

    added scatter histograms of raw data

--- examples/helpers.py
+++ examples/helpers.py
@@ -7,6 +7,7 @@ import numpy as np
 import pylab as plt
 from matplotlib import cm
 from mpl_toolkits.mplot3d import axes3d, art3d
+from matplotlib.ticker import NullFormatter
 
 
 import GPy   
@@ -51,6 +52,56 @@ def plot_xy_err_2d(indVar, dx, dy, xLabel='', yLabel=''):
     ax.set_xlabel(xLabel)
     ax.set_ylabel(yLabel)
     ax.set_title('y error')
+
+    
+def scatter_hist(x,y,xlab='',ylab=''):
+    # Taken from matplotlib examples:
+    # http://matplotlib.org/examples/pylab_examples/scatter_hist.html
+    nullfmt   = NullFormatter()         # no labels
+
+    # definitions for the axes
+    left, width = 0.1, 0.65
+    bottom, height = 0.1, 0.65
+    bottom_h = left_h = left+width+0.02
+
+    rect_scatter = [left, bottom, width, height]
+    rect_histx = [left, bottom_h, width, 0.2]
+    rect_histy = [left_h, bottom, 0.2, height]
+
+    # start with a rectangular Figure
+    plt.figure(1, figsize=(8,8))
+
+    axScatter = plt.axes(rect_scatter)
+    axHistx = plt.axes(rect_histx)
+    axHisty = plt.axes(rect_histy)
+
+    # no labels
+    axHistx.xaxis.set_major_formatter(nullfmt)
+    axHisty.yaxis.set_major_formatter(nullfmt)
+
+    # the scatter plot:
+    axScatter.scatter(x, y)
+
+    if len(xlab): axScatter.set_xlabel(xlab)
+    if len(ylab): axScatter.set_ylabel(ylab)
+
+    # now determine nice limits by hand:
+    binwidth = 0.25
+    xymax = np.max( [np.max(np.fabs(x)), np.max(np.fabs(y))] )
+    lim = ( int(xymax/binwidth) + 1) * binwidth
+
+    #axScatter.set_xlim( (-lim, lim) )
+    #axScatter.set_ylim( (-lim, lim) )
+
+    #bins = np.arange(-lim, lim + binwidth, binwidth)
+    bins = 20 #  mjp
+    axHistx.hist(x, bins=bins)
+    axHisty.hist(y, bins=bins, orientation='horizontal')
+
+    axHistx.set_xlim( axScatter.get_xlim() )
+    axHisty.set_ylim( axScatter.get_ylim() )
+
+    plt.show()
 
     
 #-------------------------------------------------------------------------------
